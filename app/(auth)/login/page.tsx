98480f847776be6d309314d6be14ddbb8dfcd4cf
kien14502 <phankien.epu@gmail.com>
2025-09-30 09:59:29 +0700

    refactor middleware for worflow authentication
    Reset .gitignore and re-apply rules
    
    feat: test husky
    
    feat: test husky
    
    fix: husky
    
    feat: test husky

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,22 +1,19 @@
-import React from "react";
-import LoginForm from "./LoginForm";
-import { Col, Row } from "antd";
-import Image from "next/image";
+import React from 'react';
+import LoginForm from './LoginForm';
+import { Col, Row } from 'antd';
+import Image from 'next/image';
 
 function LoginPage() {
   return (
     <Row>
-      <Col
-        span={12}
-        style={{ position: "relative", height: "100vh", width: "100%" }}
-      >
-        <Image
+      <Col span={12} style={{ position: 'relative', height: '100vh', width: '100%' }}>
+        {/* <Image
           src="/logo_register.png"
           alt="logo_register"
           fill
           style={{ objectFit: "cover", objectPosition: "center" }}
           priority
-        />
+        /> */}
       </Col>
       <Col span={12}>
         <LoginForm />
